64d0f819059c5ebd216ec3668006c9adbe10d0d8
ushatynybay <usharani.j@tynybay.io>
2023-02-16 14:58:06 +0530

    todo-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,215 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getLocalItems = () =>{
+  let list = localStorage.getItem('listData'); 
+
+  if(list){
+    return JSON.parse(localStorage.getItem('listData'));
+  }else{
+return [];
+  }
 }
 
-export default App;
+const Home = () => {
+  const [activity, setActivity] = useState("");
+  const [listData, setListData] = useState(getLocalItems());
+  const [status, setStatus] = useState("Incomplete");
+  const [selectedId, setSelectedId] = useState(0);
+  const [data, setData] = useState([]);
+
+  const addActivity = () => {
+    if (activity) {
+      setData([
+        ...data,
+        { activity: activity, status: status, id: data.length  },
+      ]);
+      setListData([
+        ...listData,
+        { activity: activity, status: status, id: data.length + 1 },
+      ]);
+      toast("ITEM added successfully!");
+      setActivity("");
+      setStatus("Incomplete");
+    } else {
+      window.alert("Please Enter TODO")
+    }
+  };
+
+  const deleteItem = (e) => {
+    const filterData = data.filter((data) => data.id !== Number(e.target.id));
+    setListData(filterData);
+    setData(filterData);
+    toast("ITEM Deleted successfully!");
+  };
+  const editItem = (e) => {
+    const item = data.filter((d) => d.id === Number(e.target.id));
+    setActivity(item[0].activity);
+    setStatus(item[0].status);
+    setSelectedId(Number(e.target.id));
+  };
+  const filterStatus = (status) => {
+    if (status !== "All") {
+      setListData(data.filter((x) => x.status === status));
+      console.log(status);
+    } else {
+      setListData(data);
+    }
+  };
+
+  const updateActivity = (id) => {
+    data.forEach((item) => {
+      if (item.id === id) {
+        item.activity = activity;
+        item.status = status;
+      }
+    });
+    setData(data);
+    setListData(data);
+    setSelectedId(0);
+    toast("ITEM Updated successfully!");
+    setActivity("");
+    setStatus("Incomplete");
+  };
+
+  useEffect(() => {
+    localStorage.setItem('listData', JSON.stringify(listData))
+    console.log('hi')
+  }, [listData]);
+
+  return (
+    <>
+      <div className="container">
+        <h1> TODO-LIST </h1>
+      </div>
+
+      <div className="Header">
+        <div className="add-task">
+          <button
+            type="button"
+            className="btn btn-primary"
+            data-toggle="modal"
+            data-target="#addTaskModal"
+          >
+            Add Task
+          </button>
+        </div>
+        <div className="option">
+          <select
+            className="option-head"
+            id="status"
+            onChange={(e) => {
+              filterStatus(e.target.value);
+            }}
+          >
+            <option value="All">All</option>
+            <option value="Incomplete">Incomplete</option>
+            <option value="Complete">Complete</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="body-part">
+        {listData.length === 0 ? <p> no records </p> : ""}
+        {listData !== [] &&
+          listData.map((item, i) => {
+            return (
+              <div className="title" key={item.id}>
+                <div className="checkbox">
+                  <input
+                    type="checkbox"
+                    className="main-checkbox"
+                    checked={item.status === "Complete" ? true : false}
+                  />
+                  <div className="activity-text">
+                  {item.status === "Incomplete" && item.activity}
+                  {item.status === "Complete" && <s> {item.activity}</s>}
+                  </div>
+                </div>
+                <div>
+                  <button
+                    className="button-edit"
+                    id={item.id}
+                    onClick={(e) => editItem(e)}
+                    data-toggle="modal"
+                    data-target="#addTaskModal"
+                  >
+                    <i className="fa-solid fa-pen-to-square" id={item.id}></i>
+                  </button>
+                  <button
+                    className="button-del"
+                    id={item.id}
+                    onClick={(e) => deleteItem(e)}
+                  >
+                    <i className="fa-solid fa-trash" id={item.id}></i>
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+
+      <ToastContainer position="top-right" />
+
+      <div className="modal" id="addTaskModal" tabIndex="-1" role="dialog">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              {selectedId !== 0 && <h5 className="modal-title">update TODO</h5>}
+              {selectedId === 0 && <h5 className="modal-title">ADD TODO</h5>}
+            </div>
+            <div className="modal-body">
+              <h2>Title</h2>
+              <div>
+                <input
+                  type="text"
+                  required
+                  className="input-text"
+                  value={activity}
+                  onChange={(e) => setActivity(e.target.value)}
+                />
+              </div>              
+              <div>
+                <h3>Status</h3>
+              </div>
+              <select
+                value={status}
+                className="option-new"
+                onChange={(e) => setStatus(e.target.value)}
+              >
+                <option value="Incomplete">Incomplete</option>
+                <option value="Complete">Complete</option>
+              </select>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="submit"
+                className="btn btn-primary"              
+                data-dismiss={activity.length === 0 ? "" : "modal"}
+                onClick={
+                  selectedId === 0
+                    ? () => addActivity()
+                    : () => updateActivity(selectedId)
+                }
+              >
+                {selectedId === 0 ? "Add" : "Update"} Task
+              </button>
+              <button
+                type="submit"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
